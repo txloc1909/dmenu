ff5d67a41b23aee464f16cb260754c17eebf61f3
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2022-05-06 01:35:08 +0700

    change height to 24 to match dwm bar

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 20;
+static unsigned int lineheight = 24;
 static unsigned int min_lineheight = 8;
 
 /*
@@ -25,4 +25,3 @@ static unsigned int min_lineheight = 8;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
